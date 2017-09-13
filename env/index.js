14ab21a6b8b37a9804074f706fbd056c53ce0477
sunyongjian <704928775@qq.com>
2017-09-13 20:33:11 +0800

    feat: env

---
+++ env/index.js
@@ -0,0 +1,23 @@
+const React = require('react');
+const elements = [1, 2, 3].map((item) => {
+  return (
+    <div>{item}</div>
+  )
+});
+
+console.log(elements);
+
+async function a() {
+  console.log('begin');
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000)
+  })
+  console.log('done');
+}
+a();
+
+console.log(Object.values({ 1: 2 }));
+
+console.log(Array.isArray([]));
---
+++ env/readme.md
@@ -0,0 +1,6 @@
+## 介绍
+通过配置 env 的 options，并查看编译的结果
+
+- debug
+
+![preset-env](./img/preset-env-0)
