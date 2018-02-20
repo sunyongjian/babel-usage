30cd5acbe8d94af04f081d5a74c75cb21b10fe99
sunyongjian <704928775@qq.com>
2018-02-20 20:30:05 +0800

    fix: auto commit

---
+++ copy-webpack/index.js
@@ -0,0 +1,12 @@
+async function a() {
+  console.log('begin');
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000)
+  })
+  console.log('done');
+}
+a();
+
+console.log(Object.values({ 1: 2 }));
