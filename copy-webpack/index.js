2179b4d2318e7a52decaf610e7df0a2523431e06
sunyongjian <704928775@qq.com>
2018-02-23 20:45:23 +0800

    fix: auto commit

--- copy-webpack/index.js
+++ copy-webpack/index.js
@@ -1,12 +1,1 @@
-async function a() {
-  console.log('begin');
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000)
-  })
-  console.log('done');
-}
-a();
-
-console.log(Object.values({ 1: 2 }));
+console.log('i am index');
--- copy-webpack/webpack.config.js
+++ copy-webpack/webpack.config.js
@@ -1,3 +1,20 @@
 
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'a.js',
+        to: 'dist.js',
+      },
+    ]),
+  ]
+}
